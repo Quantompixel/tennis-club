2998c57de42b952189ce735310cb171d555b4859
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2022-03-31 15:39:52 +0200

    added password repeating when signing up

--- website/signup/signup.php
+++ website/signup/signup.php
@@ -18,18 +18,18 @@ session_start();
             </div>
             <div class="form-elem">
                 <label for="lastname">Nachname: </label>
-                <input type="text" name="lastname"  id="lastname">
+                <input type="text" name="lastname" id="lastname">
             </div>
             <div class="form-elem">
                 <label for="username">Benutzername: </label>
-                <input type="text" name="username"  id="username">
+                <input type="text" name="username" id="username">
             </div>
             <div class="form-elem">
                 <label for="password">Passwort: </label>
-                <input type="password" name="password"  id="password">
+                <input type="password" name="password" id="password">
             </div>
             <div class="form-elem">
-                <input type="password" placeholder="Passwort wiederholen ..." id="repeat">
+                <input type="password" name="repeat" placeholder="Passwort wiederholen ..." id="repeat">
             </div>
             <div class="form-elem">
                 <label for="email">E-Mail-Adresse: </label>
@@ -38,7 +38,22 @@ session_start();
             <div class="form-elem">
                 <label for="phone">Telefonnummer: </label>
                 <input type="phone" name="phone" id="phone">
-            </div>
+            </div
+
+            <?php
+            $error = empty($_REQUEST['error']) ? 'empty' : $_REQUEST['error'];
+            switch ($error) {
+                case 3:
+                    echo "<p class='error'>Es sind nicht alle Felder ausgefüllt.</p>";
+                    break;
+                case 4:
+                    echo "<p class='error'>Not matching passwords.</p>";
+                    break;
+                case 10:
+                    echo "<p class='error'>SQL Error.</p>";
+                    break;
+            }
+            ?>
 
             <hr>
 
--- website/signup/submit.php
+++ website/signup/submit.php
@@ -4,6 +4,7 @@ $firstname = $_REQUEST['firstname'];
 $lastname = $_REQUEST['lastname'];
 $username = $_REQUEST['username'];
 $password = $_REQUEST['password'];
+$repeat = $_REQUEST['repeat'];
 $email = $_REQUEST['email'];
 $phone = $_REQUEST['phone'];
 
@@ -12,11 +13,16 @@ $pdo = new PDO('mysql:host=localhost;dbname=tennisclub', 'root', '');
 if (check($firstname) && check($lastname) && check($username) && check($password) && check($email)) {
     $statement = $pdo->prepare("INSERT INTO club_member (username, password, email, phone, first_name, last_name) VALUES (:username, MD5(:password), :email, :phone, :first_name, :last_name)");
 
-    if ($statement->execute(array('username' => $username, 'password' => $password, 'email' => $email, 'phone' => $phone, 'first_name' => $firstname, 'last_name' => $lastname))) {
-        header('Location: /');
+    if (strcmp($repeat, $password) == 0) {
+        if ($statement->execute(array('username' => $username, 'password' => $password, 'email' => $email, 'phone' => $phone, 'first_name' => $firstname, 'last_name' => $lastname))) {
+            header('Location: /');
+        } else {
+            // ERROR 10: SQL Error.
+            header('Location: /signup/signup.php?error=10');
+        }
     } else {
-        // ERROR 10: SQL Error.
-        header('Location: /signup/signup.php/?error=10');
+        // ERROR 4: Not matching passwords.
+        header('Location: /signup/signup.php?error=4');
     }
 } else {
     // ERROR 3: Empty parameters.
